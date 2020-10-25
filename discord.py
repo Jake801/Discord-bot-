d4e99a91bf61b5685d931338b3af48806644884e
SoonTM 32 <Jakebull123456@outlook.com>
2020-10-25 08:49:41 +0000

    Created file for discord bot

---
+++ discord.py
@@ -0,0 +1,10 @@
+import discord 
+from discord.ext import commands
+bot = commands.Bot(command_prefix= '.')
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('type .help'))
+    print("Bot is online")
+
+bot.run('Your token goes here')
