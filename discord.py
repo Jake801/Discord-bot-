cf462ec5dd0295d8eb7b25c7ac197a4010268a28
SoonTM 32 <Jakebull123456@outlook.com>
2020-10-25 08:52:26 +0000

    Update discord.py

--- discord.py
+++ discord.py
@@ -7,4 +7,8 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('type .help'))
     print("Bot is online")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+    
 bot.run('Your token goes here')
